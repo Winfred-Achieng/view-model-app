036a66af7f9f50f131c85f47d73713cba31bf792
winfred <achiengwinfred20@gmail.com>
2024-03-25 12:01:27 +0300

    viewmodel class

--- app/src/main/java/com/winfred/viewmodelapp/MainActivity.java
+++ app/src/main/java/com/winfred/viewmodelapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.winfred.viewmodelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.winfred.viewmodelapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    MyViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        binding.btnInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.increaseCounter();
+                binding.tvCounter.setText(""+ viewModel.getCounter());
+            }
+        });
+
+        binding.tvCounter.setText("" +viewModel.getCounter());
+
     }
 }
---
+++ app/src/main/java/com/winfred/viewmodelapp/MyViewModel.java
@@ -0,0 +1,16 @@
+package com.winfred.viewmodelapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+
+    int counter = 0;
+
+    public void increaseCounter(){
+        counter++;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
